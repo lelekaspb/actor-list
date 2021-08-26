5e1bef6887f106219c1492ed283c8b36eeaf89e5
lelekaspb <bov-1986@yandex.ru>
2021-08-26 19:02:33 +0200

    added filter

--- js/script.js
+++ js/script.js
@@ -3,6 +3,11 @@ window.addEventListener("load", start);
 const actorTemplate = document.querySelector("template").content;
 const actorList = document.querySelector(".actors");
 const modal = document.querySelector(".modal_wrapper");
+const filterBtn = document.querySelector("#movie-button");
+const dropdown = document.querySelector("#movie-dropdown");
+const dropdownList = document.querySelectorAll("#movie-dropdown > a");
+let dataArray = [];
+let filtered = [];
 
 const options = {
   method: "GET",
@@ -21,6 +26,7 @@ function start() {
 }
 
 function renderActors(actors) {
+  actorList.innerHTML = "";
   actors.forEach((actor, index) => {
     const actorClone = actorTemplate.cloneNode(true);
     actorClone.querySelector(".number").textContent = index + 1;
@@ -35,7 +41,7 @@ function renderActors(actors) {
 }
 
 function showDetails() {
-  console.log(this);
+  // console.log(this);
   modal.querySelector(".movie_name > span").textContent = this.dataset.movie;
   modal.querySelector(".actor_name > span").textContent = this.dataset.actor;
   modal.style.display = "block";
@@ -43,7 +49,7 @@ function showDetails() {
 }
 
 function closeDetails(e) {
-  console.log(e.target);
+  // console.log(e.target);
   if (
     e.target.classList.contains("modal_wrapper") ||
     e.target.classList.contains("modal_close")
@@ -52,4 +58,38 @@ function closeDetails(e) {
     modal.querySelector(".actor_name > span").textContent = "";
     modal.style.display = "none";
   }
+}
+
+// filter
+filterBtn.addEventListener("click", function (e) {
+  e.stopPropagation();
+  dropdown.classList.toggle("hidden");
+});
+
+window.addEventListener("click", function () {
+  dropdown.classList.add("hidden");
+});
+
+dropdownList.forEach((link) => {
+  link.addEventListener("click", filterActors);
+});
+
+function filterActors(e) {
+  fetch("./json/actors.json", options)
+    .then((res) => res.json())
+    .then(function (data) {
+      if (e.target.innerText !== "All movies") {
+        filtered = data.filter(
+          (dataItem) => dataItem.movie === e.target.innerText
+        );
+      } else {
+        filtered = data;
+      }
+      document.querySelector("#movie-button span:nth-of-type(1)").textContent =
+        e.target.innerText;
+      renderActors(filtered);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 }
